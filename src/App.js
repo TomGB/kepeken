c9e9f8c6cda08cea31d83e5acaa6714aa00c4af1
TomGB <tgbanister@gmail.com>
2017-06-08 21:40:29 +0100

    edit and move notes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function NotesArea({notes, updateNote}) {
+function NotesArea({notes, updateNote, selectNote, editNote, deselectNote}) {
   return (
     <div className='noteContainer'>
       {notes.map((note, index) =>
-        <div className='note' key={index}>
-          <textarea className='noteText' onChange={(event) => updateNote(event, index)} autoFocus></textarea>
+        <div
+          className='note'
+          key={index}
+          onMouseDown={() => selectNote(index)}
+          style={note.style} >
+          <textarea
+            className='noteText'
+            onChange={(event) => updateNote(event, index)}
+            readOnly={!(note.editable || note.editing)}
+            onClick={(event) => editNote(event, index)}
+            onBlur={() => deselectNote(index)}>
+          </textarea>
         </div>
       )}
     </div>
@@ -32,7 +42,7 @@ class App extends Component {
     const current = history[history.length - 1];
     const notes = current.notes.slice();
 
-    notes.push({});
+    notes.push({ style: { top: 10, left: 10 } });
 
     this.setState({
       history: history.concat([
@@ -44,7 +54,7 @@ class App extends Component {
     });
   }
 
-  updateNote(event, index) {
+  updateNote (event, index) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const notes = current.notes.slice();
@@ -63,18 +73,137 @@ class App extends Component {
     });
   }
 
+  selectNote (i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const notes = current.notes.slice();
+
+    notes[i].selected = true;
+
+    this.setState({
+      history: history.concat([
+        {
+          notes: notes,
+        }
+      ]),
+      stepNumber: history.length,
+    });
+  }
+
+  moveNote (event, index) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const notes = current.notes.slice();
+
+    const note = (() => {
+      for (var i = 0; i < notes.length; i++) {
+        if (notes[i].selected) {
+          return notes[i];
+        }
+      }
+
+      return false;
+    })();
+
+    if(note && !note.editing) {
+      if(event.buttons){
+        if (note.oldX) {
+
+          note.X = event.clientX;
+          note.Y = event.clientY;
+
+          const diffX = note.X - note.oldX;
+          const diffY = note.Y - note.oldY;
+
+          note.style.left = note.style.left + diffX;
+          note.style.top = note.style.top + diffY;
+
+          note.oldX = note.X;
+          note.oldY = note.Y;
+        } else {
+          note.oldX = event.clientX;
+          note.oldY = event.clientY;
+        }
+      } else {
+        this.releaseNote();
+      }
+
+      this.setState({
+        history: history.concat([
+          {
+            notes: notes,
+          }
+        ]),
+        stepNumber: history.length,
+      });
+    }
+  }
+
+  releaseNote () {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const notes = current.notes.slice();
+
+    notes.forEach(note => {
+      note.selected = false;
+      note.oldX = null;
+      note.oldY = null;
+    });
+  }
+
+  editNote (event, i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const notes = current.notes.slice();
+
+    if (!notes[i].editing && notes[i].editable) {
+      notes[i].editing = true;
+
+      console.log('editing');
+    }
+
+    if (!notes[i].editing && !notes[i].editable) {
+      notes[i].editable = true;
+
+      console.log('editable');
+
+      setTimeout(() => {
+        notes[i].editable = false;
+        console.log('not editable');
+      }, 500);
+    }
+  }
+
+  deselectNote (i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const notes = current.notes.slice();
+
+    notes[i].editing = false;
+    notes[i].editable = false;
+
+    console.log('deselect');
+  }
+
   render() {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
 
     return (
-      <div className='main'>
+      <div className='main' onMouseMove={(event) => this.moveNote(event)} onMouseUp={() => this.releaseNote()}>
         <div className='menu'>
           <button onClick={() => this.createNote()} className='createNote' title='create note'>
             create note
           </button>
         </div>
-        <NotesArea notes={current.notes} className='NotesArea' updateNote={(event, index) => this.updateNote(event, index)}/>
+        <NotesArea
+          notes={current.notes}
+          className='NotesArea'
+          updateNote={(event, index) => this.updateNote(event, index)}
+          selectNote={(index) => this.selectNote(index)}
+          editNote={(event, index) => this.editNote(event, index)}
+          deselectNote={(index) => this.deselectNote(index)}
+        />
       </div>
     );
   }
